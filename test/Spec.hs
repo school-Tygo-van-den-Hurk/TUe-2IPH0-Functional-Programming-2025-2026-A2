ba413b00e05cfaec727141f3f56d08a1a9f71385
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-06-15 19:54:44 +0200

    test: add remaining test cases
    They require 2 test cases per function.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,6 @@
--- \|
 -- Module      : PegSolitaire
 -- Description :
 -- Copyright   : Tygo van den Hurk (1705709)
---               Kylian Maas (1712861)
 -- Date:       : 2026-06-02
 -- License     : None
 import Control.Exception (evaluate)
@@ -13,12 +11,12 @@ import Test.QuickCheck
 
 main :: IO ()
 main = hspec $ do
+  let o = Empty :: Peg
+  let x = Peg :: Peg
+
   -- ~~~ Exercise 1 ~~~ --
 
   describe "isWinning" $ do
-    let o = Empty :: Peg
-    let x = Peg :: Peg
-
     it "X.X.X.X : not Win" $ do
       isWinning [x, o, x, o, x, o, x] `shouldBe` False
     it "X...... : Win" $ do
@@ -35,17 +33,17 @@ main = hspec $ do
 
     describe "foldT" $ do
       it "sumT" $ do
-        let sumT = foldT id (\(x, y) -> x + sum y)
+        let sumT = foldT id (\(a, b) -> a + sum b)
         sumT tree `shouldBe` (7 :: Integer)
       it "elemT" $ do
-        let elemT x = foldT (== x) (\(y, z) -> (y == x) || or z)
+        let elemT a = foldT (== a) (\(b, c) -> (b == a) || or c)
         (1 `elemT` tree) `shouldBe` True
         (2 `elemT` tree) `shouldBe` True
         (3 `elemT` tree) `shouldBe` False
         (4 `elemT` tree) `shouldBe` True
         (5 `elemT` tree) `shouldBe` False
       it "mapT" $ do
-        let mapT f = foldT (Leaf . f) (\(x, y) -> Node (f x) y)
+        let mapT f = foldT (Leaf . f) (\(y, z) -> Node (f y) z)
         mapT (+ 1) tree `shouldBe` Node 5 [Leaf 2, Leaf 3]
         mapT even tree `shouldBe` Node True [Leaf False, Leaf True]
 
@@ -124,32 +122,41 @@ main = hspec $ do
       it "`tryLeft` on `zipper` is `Nothing`" $ do
         tryLeft zipper `shouldBe` Nothing
       it "`tryRight` on `rightZipper` is `Nothing`" $ do
-        tryRight Zipper {
-          left = ['b', 'c', 'd', 'e'],
-          current = 'a',
-          right = []
-        } `shouldBe` Nothing
+        tryRight
+          Zipper
+            { left = ['b', 'c', 'd', 'e'],
+              current = 'a',
+              right = []
+            }
+          `shouldBe` Nothing
 
   -- ~~~ Exercise 4 ~~~ --
 
   describe "generateStates" $ do
     it "generateStates 0 == []" $ do
-      generateStates 0 `shouldBe` [[]]
+      generateStates 0 `shouldBe` []
     it "generateStates 1 is a set of [Empty] and [Peg]" $ do
       let result = generateStates 1
-      length result `shouldBe` 2
       result `shouldSatisfy` elem [Peg]
       result `shouldSatisfy` elem [Empty]
-    it "generateStates 2 contains all 4 combinations" $ do
+      length result `shouldBe` 2
+    it "generateStates 2 contains all 6 combinations" $ do
       let result = generateStates 2
-      length result `shouldBe` 4
+      result `shouldSatisfy` elem [Empty]
+      result `shouldSatisfy` elem [Peg]
       result `shouldSatisfy` elem [Empty, Empty]
       result `shouldSatisfy` elem [Empty, Peg]
       result `shouldSatisfy` elem [Peg, Empty]
       result `shouldSatisfy` elem [Peg, Peg]
-    it "generateStates 3 contains all 8 combinations" $ do
+      length result `shouldBe` 6
+    it "generateStates 3 contains all 14 combinations" $ do
       let result = generateStates 3
-      length result `shouldBe` 8
+      result `shouldSatisfy` elem [Empty]
+      result `shouldSatisfy` elem [Peg]
+      result `shouldSatisfy` elem [Empty, Empty]
+      result `shouldSatisfy` elem [Empty, Peg]
+      result `shouldSatisfy` elem [Peg, Empty]
+      result `shouldSatisfy` elem [Peg, Peg]
       result `shouldSatisfy` elem [Empty, Empty, Empty]
       result `shouldSatisfy` elem [Empty, Empty, Peg]
       result `shouldSatisfy` elem [Empty, Peg, Empty]
@@ -158,43 +165,91 @@ main = hspec $ do
       result `shouldSatisfy` elem [Peg, Empty, Peg]
       result `shouldSatisfy` elem [Peg, Peg, Empty]
       result `shouldSatisfy` elem [Peg, Peg, Peg]
-    it "generateStates 3 contains all 8 combinations" $ do
-      let result = generateStates 3
-      length result `shouldBe` 8
-      result `shouldSatisfy` elem [Empty, Empty, Empty]
-      result `shouldSatisfy` elem [Empty, Empty, Peg]
-      result `shouldSatisfy` elem [Empty, Peg, Empty]
-      result `shouldSatisfy` elem [Empty, Peg, Peg]
-      result `shouldSatisfy` elem [Peg, Empty, Empty]
-      result `shouldSatisfy` elem [Peg, Empty, Peg]
-      result `shouldSatisfy` elem [Peg, Peg, Empty]
-      result `shouldSatisfy` elem [Peg, Peg, Peg]
+      length result `shouldBe` 14
 
   describe "generateLinearStates" $ do
-    it "should have tests" $ do
-      (1 :: Integer) `shouldBe` (1 :: Integer)
+    it "generateLinearStates 3 produces all single-empty states of length 3" $ do
+      map (concatMap show) (generateLinearStates 3) `shouldBe` [".XX", "X.X", "XX."]
+
+    it "generateLinearStates 4 produces 4 states each with exactly one Empty" $ do
+      let states = generateLinearStates 4
+      length states `shouldBe` 4
+      all (\ps -> length (filter (== Empty) ps) == 1 && length ps == 4) states `shouldBe` True
 
   -- ~~~ Exercise 5 ~~~ --
 
   describe "makeMoves" $ do
-    it "should have tests" $ do
-      (1 :: Integer) `shouldBe` (1 :: Integer)
+    it "makeMoves on 'XX.' gives one move resulting in '..X'" $ do
+      map fromZipper (makeMoves (toZipper (stringToPegs "XX."))) `shouldBe` [stringToPegs "..X"]
+    it "makeMoves on 'X.X' gives no moves" $ do
+      makeMoves (toZipper (stringToPegs "X.X")) `shouldBe` []
+
+  -- ~~~ Exercise 6 ~~~ --
 
   describe "unfoldT" $ do
-    it "should have tests" $ do
-      (1 :: Integer) `shouldBe` (1 :: Integer)
+    it "unfoldT builds the expected tree from seed 3" $ do
+      let rec a = if a <= 0 then Left 0 else Right (a, [a - 1, a - 2])
+      unfoldT rec (3 :: Integer) `shouldBe` Node 3 [Node 2 [Node 1 [Leaf 0, Leaf 0], Leaf 0], Node 1 [Leaf 0, Leaf 0]]
+    it "unfoldT on a Left seed produces a single Leaf" $ do
+      let rec a = if a <= 0 then Left a else Right (a, [a - 1])
+      unfoldT rec (0 :: Integer) `shouldBe` Leaf 0
+
+  -- ~~~ Exercise 7 ~~~ --
 
   describe "makeGameTree" $ do
-    it "should have tests" $ do
-      (1 :: Integer) `shouldBe` (1 :: Integer)
+    let mapT f = foldT (Leaf . f) (\(y, z) -> Node (f y) z)
+    let fromZipperT = mapT fromZipper
+    it "Game Tree of 'X.X'" $ do
+      let game = toZipper $ stringToPegs "X.X"
+      let tree = makeGameTree game
+      fromZipperT tree `shouldBe` Leaf [x, o, x]
+    it "Game Tree of 'X.XX'" $ do
+      let game = toZipper $ stringToPegs "X.XX"
+      let tree = makeGameTree game
+      fromZipperT tree `shouldBe` Node [x, o, x, x] [Node [x, x, o, o] [Leaf [o, o, x, o]]]
+    it "Game Tree of '.X.XX'" $ do
+      let game = toZipper $ stringToPegs ".X.XX"
+      let tree = makeGameTree game
+      fromZipperT tree `shouldBe` Node [o, x, o, x, x] [Node [o, x, x, o, o] [Leaf [o, o, o, x, o], Leaf [x, o, o, o, o]]]
+
+  -- ~~~ Exercise 8 ~~~ --
 
   describe "hasSolution" $ do
-    it "should have tests" $ do
-      (1 :: Integer) `shouldBe` (1 :: Integer)
+    it "No solutions for 'X.X'" $ do
+      let game = stringToPegs "X.X"
+      hasSolution game `shouldBe` False
+    it "One Solution for 'XX.'" $ do
+      let game = stringToPegs "XX."
+      hasSolution game `shouldBe` True
+    it "One Solution for 'X.XX'" $ do
+      let game = stringToPegs "X.XX"
+      hasSolution game `shouldBe` True
+    it "No Solutions for 'X..XX'" $ do
+      let game = stringToPegs "X..XX"
+      hasSolution game `shouldBe` False
+
+  -- ~~~ Exercise 9 ~~~ --
 
   describe "allSolutions" $ do
-    it "should have tests" $ do
-      (1 :: Integer) `shouldBe` (1 :: Integer)
+    it "No solutions for 'X.X'" $ do
+      let game = stringToPegs "X.X"
+      let none = []
+      allSolutions game `shouldBe` none
+    it "One Solution for '.XX'" $ do
+      let game = stringToPegs ".XX"
+      let solution = [x, o, o]
+      allSolutions game `shouldBe` [solution]
+    describe "Two Solutions for 'XX.X.'" $ do
+      let game = stringToPegs "XX.X."
+      let solutions = allSolutions game
+      it "solution 1: '....X'" $ do
+        let solution1 = [o, o, o, o, x]
+        solutions `shouldSatisfy` elem solution1
+      it "solution 2: '.X...'" $ do
+        let solution2 = [o, x, o, o, o]
+        solutions `shouldSatisfy` elem solution2
+
+  -- ~~~ Bonus Exercise ~~~ --
 
   describe "getSolution" $ do
     it "should have tests" $ do
